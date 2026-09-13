2f3bdc736c1d2befd56b857a7c803a47608d4bac
Maram <mmaarr1420m@email.com>
2026-09-13 18:59:39 +0300

    Lab-01 Done ✅.

---
+++ First_Year/Programming-1/Lab-01/Exercise_01.java
@@ -0,0 +1,9 @@
+public class Exercise_01
+{
+    public static void main(String[] args)
+    {
+        System.out.print("Welcome ");
+        System.out.print("to ");
+        System.out.print("Java ");
+    }
+}
---
+++ First_Year/Programming-1/Lab-01/Exercise_04.java
@@ -0,0 +1,10 @@
+public class Exercise_04
+{
+    public static void main(String[] args)
+    {
+        System.out.println("   9.5 x 4.5 - 2.5 x 3    ");
+        System.out.println("_________________________ ");
+        System.out.println();
+        System.out.println("       45.5 - 3.5        ");
+    }
+}
